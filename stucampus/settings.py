473819b61515f0a97c81a9722130dccf1f4d9ba4
jimczj <jimczj@gmail.com>
2016-06-08 04:32:25 +0800

    add app summer_plans

--- stucampus/settings.py
+++ stucampus/settings.py
@@ -134,6 +134,7 @@ INSTALLED_APPS = (
     'stucampus.minivideo',
     'stucampus.carousels',
     'stucampus.member_infor',
+    'stucampus.summer_plans',
 
     #'stucampus.dreamer',
     'stucampus.FreeTimeCount',
